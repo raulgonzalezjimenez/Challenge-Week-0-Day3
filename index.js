b1f7efe51d5fab4ae77d07dc17d84cfba2a4b74c
raulgonzalez <raulgonji@gmail.com>
2024-02-14 19:03:32 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+export { strictEquals };
+
+function strictEquals(a, b) {
+  if (Object.is(a, b)) {
+    return true;
+  }
+  if (Object.is(0, b) && Object.is(a, -0)) {
+    return true;
+  }
+  if (Object.is(-0, b) && Object.is(a, 0)) {
+    return true;
+  }
+  if (Object.is(NaN, b) && Object.is(a, NaN)) {
+    return false;
+  }
+  return false;
+}
